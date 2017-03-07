f972dda4d9dad94ba1db35ec773e976602b943eb
Kenny De Smedt <de_smedt_kenny@telenet.be>
2017-03-07 18:35:47 +0100

    - First GUI setup

---
+++ gui/InfiniteInventoryGui.hpp
@@ -0,0 +1,79 @@
+////////////////////////////////////////////////////////
+// GUI EDITOR OUTPUT START (by Celludriel, v1.063, #Femuby)
+////////////////////////////////////////////////////////
+
+class InfiniteInventoryFrame: RscFrame
+{
+	idc = 1800;
+	x = 0 * GUI_GRID_W + GUI_GRID_X;
+	y = 0 * GUI_GRID_H + GUI_GRID_Y;
+	w = 40 * GUI_GRID_W;
+	h = 25 * GUI_GRID_H;
+};
+class InventoryFilter: RscCombo
+{
+	idc = 2100;
+	x = 0.5 * GUI_GRID_W + GUI_GRID_X;
+	y = 0.5 * GUI_GRID_H + GUI_GRID_Y;
+	w = 13.5 * GUI_GRID_W;
+	h = 1 * GUI_GRID_H;
+	tooltip = "Filter Items"; //--- ToDo: Localize;
+};
+class EquipmentFilter: RscCombo
+{
+	idc = 2101;
+	x = 26 * GUI_GRID_W + GUI_GRID_X;
+	y = 0.5 * GUI_GRID_H + GUI_GRID_Y;
+	w = 13.5 * GUI_GRID_W;
+	h = 1 * GUI_GRID_H;
+	tooltip = "Filter items"; //--- ToDo: Localize;
+};
+class InventoryContainer: RscListbox
+{
+	idc = 1500;
+	x = 0.5 * GUI_GRID_W + GUI_GRID_X;
+	y = 2 * GUI_GRID_H + GUI_GRID_Y;
+	w = 13.5 * GUI_GRID_W;
+	h = 22.5 * GUI_GRID_H;
+};
+class EquipmentContainer: RscListbox
+{
+	idc = 1501;
+	x = 26 * GUI_GRID_W + GUI_GRID_X;
+	y = 2 * GUI_GRID_H + GUI_GRID_Y;
+	w = 13.5 * GUI_GRID_W;
+	h = 22.5 * GUI_GRID_H;
+};
+class AddSingleItemButton: RscButton
+{
+	idc = 1600;
+	text = "<"; //--- ToDo: Localize;
+	x = 15.5 * GUI_GRID_W + GUI_GRID_X;
+	y = 11.5 * GUI_GRID_H + GUI_GRID_Y;
+	w = 4 * GUI_GRID_W;
+	h = 1.5 * GUI_GRID_H;
+	tooltip = "Add"; //--- ToDo: Localize;
+};
+class TakeSingleItemButton: RscButton
+{
+	idc = 1601;
+	text = ">"; //--- ToDo: Localize;
+	x = 20 * GUI_GRID_W + GUI_GRID_X;
+	y = 11.5 * GUI_GRID_H + GUI_GRID_Y;
+	w = 4 * GUI_GRID_W;
+	h = 1.5 * GUI_GRID_H;
+	tooltip = "Take"; //--- ToDo: Localize;
+};
+class TakeAllButton: RscButton
+{
+	idc = 1602;
+	text = "<<<"; //--- ToDo: Localize;
+	x = 15.5 * GUI_GRID_W + GUI_GRID_X;
+	y = 13.5 * GUI_GRID_H + GUI_GRID_Y;
+	w = 8.5 * GUI_GRID_W;
+	h = 1.5 * GUI_GRID_H;
+	tooltip = "Add all"; //--- ToDo: Localize;
+};
+////////////////////////////////////////////////////////
+// GUI EDITOR OUTPUT END
+////////////////////////////////////////////////////////
