2abf2d0c31f33dbd3b19609efe911f746bbc866e
Kenny De Smedt <de_smedt_kenny@telenet.be>
2017-03-08 17:40:38 +0100

    - scaffold to work on

---
+++ CfgFunctions.hpp
@@ -0,0 +1,13 @@
+class InfiniteInventoryFunctions {
+	tag = "INFINV";
+
+	class InfiniteInventoryInitialize {
+		file = "functions";
+
+		class addItemToContainer {};
+		class takeItemFromContainer {};
+		class addAllItemsToContainer {};
+		class filterContainer {};
+		class filterEquipment {};
+	};
+};
