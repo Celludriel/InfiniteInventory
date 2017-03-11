42c44e68fae5f04bf426a06fe0ac2d48f4d35f33
Kenny De Smedt <de_smedt_kenny@telenet.be>
2017-03-11 12:11:16 +0100

    - No more errors but also no dialog yet

--- gui/InfiniteInventoryDialog.hpp
+++ gui/InfiniteInventoryDialog.hpp
@@ -1,14 +1,11 @@
 class INFINV_Dialog {
-	idd = -1;
+	idd = 1900;
+	movingEnable = false;
+	enableSimulation = 1;	
 	onLoad = "";
 	onUnload = "";
-	class Controls {
-		class InfiniteInventoryFrame: RscFrame {
-			idc = 1800;
-			x = 0 * GUI_GRID_W + GUI_GRID_X;
-			y = 0 * GUI_GRID_H + GUI_GRID_Y;
-			w = 40 * GUI_GRID_W;
-			h = 25 * GUI_GRID_H;
-		};
-	};
+	controlsBackground[] = { "InfiniteInventoryFrame", "InventoryFilter", "InventoryFilter", "EquipmentFilter", 
+	"InventoryContainer", "EquipmentContainer", "AddSingleItemButton", "TakeSingleItemButton", "TakeAllButton" };
+	
+	#include "InfiniteInventoryGui.hpp" 
 };
